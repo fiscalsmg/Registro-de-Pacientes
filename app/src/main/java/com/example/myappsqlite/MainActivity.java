682b9a4c5b2b6bbaf94a0d0eb4cf0d4f923a26f9
fiscal <fiscalguadalupe422@gmail.com>
2020-05-10 22:57:48 -0500

    Se arreglo el problema de poder insertar utilizando menos lineas de codigo

--- app/src/main/java/com/example/myappsqlite/MainActivity.java
+++ app/src/main/java/com/example/myappsqlite/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.myappsqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -15,11 +15,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     EditText edt_id, edt_raza, edt_color;
     Button btn_create, btn_read, btn_update, btn_delete;
+    SQLiteDatabase BaseDeDatos = null;
+    ConexionBD admin;
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv1 = (TextView) findViewById(R.id.textView);
+
 
         edt_id = findViewById(R.id.txt_id);
         edt_raza = findViewById(R.id.txt_raza);
@@ -41,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 registrar();
                 break;
             case R.id.btn_read:
+                buscar();
                 break;
             case R.id.btn_update:
                 break;
@@ -50,34 +56,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void registrar() {
-        ConexionBD admin = new ConexionBD(this, "tortugaBD", null, 1);
-        SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();//abre bd modo leectura y escritura
+      private void registrar() {
+        try {
+            admin = new ConexionBD(this, "tortugaBD", null, 1);
+            BaseDeDatos = admin.getWritableDatabase();//abre bd modo leectura y escritura
 
-        String id = edt_id.getText().toString();
-        String raza = edt_raza.getText().toString();
-        String color = edt_color.getText().toString();
+            String id = edt_id.getText().toString();
+            String raza = edt_raza.getText().toString();
+            String color = edt_color.getText().toString();
 
-        //valida campos
-        if (!id.isEmpty() && !raza.isEmpty() && !color.isEmpty()) {
-            ContentValues registro = new ContentValues();
+            //valida campos
+            if (!id.isEmpty() && !raza.isEmpty() && !color.isEmpty()) {
+                if (BaseDeDatos != null) {
+                    BaseDeDatos.execSQL("INSERT INTO tortuga values(" + id + ",'" + raza + "','" + color + "')");
+                    BaseDeDatos.close();
+                    edt_id.setText("");
+                    edt_raza.setText("");
+                    edt_color.setText("");
 
-            //guarda los valores que el usuario ha escrito
-            registro.put("id", id);
-            registro.put("raza", raza);
-            registro.put("color", color);
+                    Toast.makeText(this, "Registro Exitoso!!", Toast.LENGTH_SHORT).show();
+                }
 
-            BaseDeDatos.insert("tortuga", null, registro);
-            BaseDeDatos.close();
-            edt_id.setText("");
-            edt_raza.setText("");
-            edt_color.setText("");
+            } else {
+                Toast.makeText(this, "Registra todos los datos", Toast.LENGTH_SHORT).show();
 
-            Toast.makeText(this, "Registro Exitoso!!", Toast.LENGTH_SHORT).show();
-
-        } else {
-            Toast.makeText(this, "Registra todos los datos", Toast.LENGTH_SHORT).show();
-
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Ups!"+e.getMessage(), Toast.LENGTH_SHORT).show();
         }
+    }
+    private void buscar() {
     }
 }
